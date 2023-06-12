c654fd5d60dc0bb28dffb70294930e6d482b74a8
Nishatnuha <48322328+Nishatnuha@users.noreply.github.com>
2023-06-12 22:58:06 +0600

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-from flask import Flask, jsonify
+from flask import Flask
 
+
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
 app = Flask(__name__)
 
-@app.route('/generate_even_numbers/<int:n>', methods=['GET'])
-def generate_even_numbers(n):
-    if n < 0:
-        return jsonify({'error': 'Input should be a non-negative number'})
-    else:
-        even_numbers = [i for i in range(0, 2 * n, 2)]
-        return jsonify({'even_numbers': even_numbers})
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    
+    a = []
+
+    for e in range(1,25,1):
+        if (e%2) == 0:
+            a.append(e)
+    return a
+
 
 if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. You
+    # can configure startup instructions by adding `entrypoint` to app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
